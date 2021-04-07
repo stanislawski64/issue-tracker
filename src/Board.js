649ed51757c063cc33a4535b28a7ea9d7bb5362f
stanislawski64 <dariusz.stanislawski64@gmail.com>
2021-04-07 14:44:33 +0200

    fixed backdrop behaviour, board is almost complete

--- src/Board.js
+++ src/Board.js
@@ -1,10 +1,24 @@
 function Board() {
   return (
-    <main className="MainContent">
-      <div className="BoardDiv"></div>
-      <div className="BoardDiv"></div>
-      <div className="BoardDiv"></div>
-      <div className="BoardDiv"></div>
+    <main className="MainContent Board">
+      <div className="CardsContainer">
+        <div className="Card">
+          <div className="CardTitleDiv">to do</div>
+          <div className="Issue">Yo it's an issue</div>
+          <div className="Issue">Yo it's an issue</div>
+          <div className="Issue">Yo it's an issue</div>
+          <div className="Issue">Yo it's an issue</div>
+        </div>
+        <div className="Card">
+          <div className="CardTitleDiv">in progress</div>
+        </div>
+        <div className="Card">
+          <div className="CardTitleDiv">in review</div>
+        </div>
+        <div className="Card">
+          <div className="CardTitleDiv">done</div>
+        </div>
+      </div>
     </main>
   );
 }
